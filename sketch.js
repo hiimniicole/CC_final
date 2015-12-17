42e7232f868be540357fa2f94be49736232e3f3e
hiimniicole <llama11024@aol.com>
2015-12-17 14:24:45 -0500

    added rooms

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
+var houseImg;
+var roomImg;
+var bathroomImg;
+var ratImg;
+function preload() {
+  houseImg = loadImage("pics/house.png");
+roomImg = loadImage("pics/room.png");
+bathroomImg = loadImage("pics/bathroom.png");
+ratImg = loadImage("pics/rat.png");
+}
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(800, 800);
 }
 
 function draw() {
-	// clear the background
-	background(0, 0, 0);
 
-	// set a fill color
-	fill(255, 255, 255);
-
-	// set a stroke color
-	stroke(255, 0, 0);
-
-	// draw a circle
-	ellipse(320, 180, 100, 100);
+	//put background here
+	image(houseImg,0,0);
 }
